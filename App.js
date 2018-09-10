ae12792ac47e0d97054a8ee76e129b2a46fa33e2
Nicolas Sugino <suja.20@gmail.com>
2018-09-10 00:53:47 -0300

    Se agregan mas botones

--- App.js
+++ App.js
@@ -7,9 +7,9 @@ import moment from 'moment';
 
 //Componente de timer
 function Timer({ interval }) {
-	const duration = moment.duration(interval);
+	const duration = moment.duration(interval)
 	return (
-		<Text style={styles.timer}> 
+		<Text style={styles.timer}>
 			{duration.minutes()}:{duration.seconds()}
 		</Text>
 	);
@@ -17,10 +17,11 @@ function Timer({ interval }) {
 
 //Componente de timer
 function Timer({ interval }) {
-	const duration = moment.duration(interval);
+	const pad = (n) => n < 10 ? '0' + n : n
+	const duration = moment.duration(interval)
 	return (
 		<Text style={styles.info}> 
-			Tiempo: {duration.minutes()}:{duration.seconds()}
+			Tiempo: {pad(duration.minutes())}:{pad(duration.seconds())}
 		</Text>
 	);
 }
@@ -35,8 +36,11 @@ function Distance({ distance }) {
 }
 
 //Componente de precio
-function Price({ cents }) {
-	const pesos = cents/100;
+function Price({ start, tiempo, distancia }) {
+	const base = 3260
+	const precioTiempo = moment.duration(tiempo).minutes()*326
+	const precioDistancia = 0
+	const pesos = start ? (base+precioTiempo+precioDistancia)/100 : 0
 	return (
 		<Text style={styles.price}> 
 			${pesos}
@@ -45,13 +49,12 @@ function Price({ cents }) {
 }
 
 //Componente de boton redondo
-function RoundButton({ title, color, background, onPress, disabled}) {
+function RoundButton({ title, color, background, onPress}) {
 	return (
 		//Lo hace semitransparente al presionar
 		<TouchableOpacity
-			//onPress = {() => !disabled && onPress}
 			onPress={onPress}
-			activeOpacity = {disabled ? 1 : 0.3}
+			activeOpacity = {0.3}
 			style={[ styles.button, { backgroundColor: background }]}
 		>
 			<Text style={[ styles.buttonTitle, {color}]}> {title} </Text>
@@ -59,7 +62,17 @@ function RoundButton({ title, color, background, onPress, disabled}) {
 	);
 }
 
-//Componente de boton redondo
+//Componente de lista de boton redondo
+function RoundButtonRow({ children }) {
+	return (
+		<View style={ styles.roundButtonRow }>
+		{children}
+		</View>
+	);
+}
+
+
+//Componente de lista de info
 function InformationRow({ children }) {
 	return (
 		<View style={ styles.informationRow }>
@@ -74,6 +87,7 @@ export default class App extends Component {
 		super(props);
 		//Lo que esta aca actualiza el render cuando se modifica
 		this.state = {
+			name: 'idle',
 			start: 0, //Tiempo de inicio del timer, ms
 			now: 0, //Tiempo actual, ms
 			distance: 0, //metros
@@ -84,38 +98,95 @@ export default class App extends Component {
 	comenzar = () => {
 		const now = new Date().getTime()
 		this.setState({
+			name: 'running',
 			start: now,
 			now: now,
 		})
-		console.log('entree ')
+		console.log('Arranca ')
 		this.timer = setInterval( () => {
 			this.setState({ now: new Date().getTime()})
 		}, 1000)
 	}
 
+	continuar = () => {
+		const now = new Date().getTime()
+		this.setState({
+			name: 'running',
+			now: now,
+		})
+		console.log('Continuar')
+		this.timer = setInterval( () => {
+			this.setState({ now: new Date().getTime()})
+		}, 1000)
+	}
+
+
+	detener = () => {
+		this.setState({
+			name: 'stopped',
+		})
+		console.log('detengo ')
+		clearInterval(this.timer)
+	}
+
+	limpiar = () => {
+		const now = new Date().getTime()
+		this.setState({
+			name: 'idle',
+			start: 0,
+			now: 0,
+		})
+		console.log('Limpio')
+	}
+
 	render() {
-		const {now, start, distance, cents} = this.state;
+		const {name, now, start, distance, cents} = this.state;
 		const timer = now - start;
 		return (
 				<View style={styles.container}>
-					<Price cents={cents}/>
+					<Price start={!!now} tiempo={timer} distancia={distance}/>
 
 					<InformationRow>
 						<Timer interval={timer}/>
 						<Distance distance={distance}/>
 					</InformationRow>
 
-					<RoundButton 
-						title='Start'
-						color='#50D167'
-						background='#1B361F'
-						onPress={this.comenzar}
-						disabled='false'
-					/>
+					{name == 'idle' && (
+						<RoundButton 
+							title='Comenzar'
+							color='#50D167'
+							background='#1B361F'
+							onPress={this.comenzar}
+						/>
+					)}
+					{name == 'running' && (
+						<RoundButton 
+							title='Detener'
+							color='#e33935'
+							background='#3c1715'
+							onPress={this.detener}
+						/>
+					)}
+					{name == 'stopped' && (
+						<RoundButtonRow>
+							<RoundButton
+								title='Continuar'
+								color='#50D167'
+								background='#1B361F'
+								onPress={this.continuar}
+							/>
+							<RoundButton 
+								title='Limpiar'
+								color='#FFFFFF'
+								background='#3d3d3d'
+								onPress={this.limpiar}
+							/>
+						</RoundButtonRow>
+					)}
 				</View>
 		);
 	}
-};
+}
 
 const styles = StyleSheet.create({
 	container: {
@@ -136,11 +207,12 @@ const styles = StyleSheet.create({
 		fontWeight: '100',
 	},
 	button: {
-		width: 80,
-		height: 80,
-		borderRadius: 40,
+		width: 120,
+		height: 120,
+		borderRadius: 60,
 		justifyContent: 'center',
 		alignItems: 'center',
+		marginTop: 50,
 	},
 	buttonTitle: {
 		fontSize: 20,
@@ -150,5 +222,10 @@ const styles = StyleSheet.create({
 		alignSelf: 'stretch',
 		justifyContent: 'space-between',
 		marginTop: 50,
+	},
+	roundButtonRow: {
+		flexDirection: 'row',
+		alignSelf: 'stretch',
+		justifyContent: 'space-between',
 	}
-});
+})
